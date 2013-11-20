b4700fb9e18c278f425028fcf9777da0fdc8390a
Sven Mulholland <mulholland@netzkultur.de>
2013-11-20 09:40:05 +0100

    refactored the autoloader includes

--- pecl-manager.php
+++ pecl-manager.php
@@ -1,12 +1,13 @@
 #!/usr/bin/env php
 <?php
 
-if (file_exists('/../vendor/autoload.php')) {
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
 	require __DIR__ . '/../vendor/autoload.php';
 }
-if (file_exists('/../autoload.php')) {
+if (file_exists(__DIR__ . '/../autoload.php')) {
 	require __DIR__ . '/../autoload.php';
 }
+
 
 /**
  * Implements the worker portions of the pecl/gearman library
